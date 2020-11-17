dcf92e45d752339ef2de90c35bdb62094cd324cb
psi <psi09@hotmail.com>
2020-11-17 16:03:27 +0530

    added feature to copy and paste infra specific conditions provided policy exists.

--- shell/infra_nr_cli/cust-nr-cli.sh
+++ shell/infra_nr_cli/cust-nr-cli.sh
@@ -5,8 +5,16 @@ set -o errexit # exit the script if any statement returns a non-true return valu
 comment
 #Setting up the default value if not set.
 : "${FILE:="jqdata.json"}"
+: "${FILE_PAGINATION:="frompage"}"
+: "${offset:=0}"
 : "${UPDATE_FILE:="updatedjqdata.json"}"
 : "${CONDITION_LIST_FOR_TO_POLICY:="topolicy.json"}"
+: "${CONDITION_LIST_FOR_TO_POLICY_PAGINATION:="topage"}"
+: "${no_of_record_per_page:=50}"
+: "${loop_number:=0}"
+: "${UPDATE_FILE_PAGINATION:="updatepage"}"
+: "${offsetto:=0}"
+: "${offsetfrom:=0}"
 #set -x
 main() {
     echo "Starting the infra specific conditiopns show..Copy from one AC to another AC."
@@ -18,60 +26,113 @@ main() {
 process_request() {
     echo "processing the request.."
     check_args "$@"
-    create_dataset
-    loop_number=`cat $FILE | python -m json.tool | grep -w type | wc -l`
-    echo $loop_number
-    local h3=`cat $FILE | sed "s/$fromoneacpolicyid/$toanotheracpolicyid/g" > $UPDATE_FILE`
-    echo "restoring infra conditions to a policy id in different NR account.."
-    for (( i=0; i<$loop_number; i++ ))
-    do
-        local testdata=`cat $UPDATE_FILE | jq ".data[$i]"`
-        echo "$testdata"
-        oldname=`cat $FILE | jq ".data[$i].name"`
-        oldconid=`cat $FILE | jq ".data[$i].id"`
-        echo "$oldname:$oldconid" > oldpolicyconditionlist
-        condition_list_data=`cat $CONDITION_LIST_FOR_TO_POLICY | jq '.data[]'`
-        if [ "$condition_list_data" != "" ]
-        then
-            newname=`cat $CONDITION_LIST_FOR_TO_POLICY | jq ".data[$i].name"`
-            newconid=`cat $CONDITION_LIST_FOR_TO_POLICY | jq ".data[$i].id"`
-            echo "$newname:$newconid" > newpolicyconditionlist
-            local h1=`cat newpolicyconditionlist`
-            echo "$h1"
-            while read line
-            do
-                result_key_spaced=`echo $line | cut -d ':' -f 1`
-                result_key=`echo ${result_key_spaced//[[:blank:]]/}`
-                result_value_spaced=`echo $line | cut -d ':' -f 2`
-                result_value=`echo ${result_value_spaced//[[:blank:]]/}`
-            done < newpolicyconditionlist 
-                local h2=`cat oldpolicyconditionlist | grep -w $result_key`
-                if [ "$?" = 0 ]
-                then
-                    if [ "$result_value" != "null" ]
-                    then
-                        local result=`curl -X PUT "https://infra-api.newrelic.com/v2/alerts/conditions/$result_value" -H "X-Api-Key:$NEW_RELIC_ANOTHER_AC" -i -H 'Content-Type: application/json' -d "{\"data\": $testdata}"`
-                    fi
-                else
-                    local result=$(curl -X POST 'https://infra-api.newrelic.com/v2/alerts/conditions' -H "X-Api-Key:$NEW_RELIC_ANOTHER_AC" -i -H 'Content-Type: application/json' -d "{\"data\": $testdata}")
-                fi
-            echo "No of condiftion created for policy id: $toanotheracpolicyid is" `expr $i + 1`
-        else
-            local result=$(curl -X POST 'https://infra-api.newrelic.com/v2/alerts/conditions' -H "X-Api-Key:$NEW_RELIC_ANOTHER_AC" -i -H 'Content-Type: application/json' -d "{\"data\": $testdata}")
-        fi
-    done
+    create_dataset_if_more_page
     echo "cleaning up.."
     cleanup
 }
-create_dataset() {
-    echo "Downloading reference data from https://infra-api.newrelic.com/v2/alerts/conditions"
-    $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ONE_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$fromoneacpolicyid" > $FILE)
-    $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ONE_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$toanotheracpolicyid" > $CONDITION_LIST_FOR_TO_POLICY)
-	echo "Download dataset completed"
+create_dataset_if_more_page() {
+        echo "Checking if pagination can fetch some more records.."
+        $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ANOTHER_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$toanotheracpolicyid" > $CONDITION_LIST_FOR_TO_POLICY)
+        total_to=`cat $CONDITION_LIST_FOR_TO_POLICY | python -m json.tool | jq '.meta.total'`
+        ceiling_result_to=`echo "($total_to + $no_of_record_per_page - 1) / $no_of_record_per_page" | bc`
+        echo "ceiling_result_to = $ceiling_result_to"
+        if [ "$ceiling_result_to" != "0" ]
+        then
+                for (( i=1; i<=$ceiling_result_to; i++ )) 
+                do 
+                    $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ANOTHER_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$toanotheracpolicyid&offset=$offsetto&list=50" > $CONDITION_LIST_FOR_TO_POLICY_PAGINATION-$i.json)
+                    offsetto=`echo "$offset + 50" | bc`
+                    loop_to=`cat $CONDITION_LIST_FOR_TO_POLICY_PAGINATION-$i.json | python -m json.tool | grep -w type | wc -l`
+                    for (( j=0; j<$loop_to; j++ ))
+                    do
+                        newname_spaced=`cat $CONDITION_LIST_FOR_TO_POLICY_PAGINATION-$i.json | jq ".data[$j].name"`
+                        newconid_spaced=`cat $CONDITION_LIST_FOR_TO_POLICY_PAGINATION-$i.json | jq ".data[$j].id"`
+                        newname=`echo ${newname_spaced//[[:blank:]]/}`
+                        newconid=`echo ${newconid_spaced//[[:blank:]]/}`
+                        echo "$newname:$newconid" >> newpolicyconditionlist-$i
+                    done
+                done
+                $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ONE_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$fromoneacpolicyid" > $FILE)
+                #loop_number=`cat $FILE | python -m json.tool | grep -w type | wc -l`
+                loop_number=`cat $FILE | python -m json.tool | jq '.meta.total'`
+                ceiling_result=`echo "($loop_number + $no_of_record_per_page - 1) / $no_of_record_per_page" | bc`
+                for (( i=1; i<=$ceiling_result; i++ )) 
+                do
+                        $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ONE_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$fromoneacpolicyid&offset=$offsetfrom&list=50" > $FILE_PAGINATION-$i.json)
+                        offsetfrom=`echo "$offset + 50" | bc`
+                        h3=`cat $FILE_PAGINATION-$i.json | sed "s/$fromoneacpolicyid/$toanotheracpolicyid/g" > $UPDATE_FILE_PAGINATION-$i.json`
+                        loop=`cat $UPDATE_FILE_PAGINATION-$i.json | python -m json.tool | grep -w type | wc -l`
+                        for (( j=0; j<$loop; j++ ))
+                        do
+                            oldname_spaced=`cat $FILE_PAGINATION-$i.json | jq ".data[$j].name"`
+                            oldconid_spaced=`cat $FILE_PAGINATION-$i.json | jq ".data[$j].id"`
+                            oldname=`echo ${oldname_spaced//[[:blank:]]/}`
+                            oldconid=`echo ${oldconid_spaced//[[:blank:]]/}`
+                            echo "$oldname:$oldconid" >> oldpolicyconditionlist
+                        done
+                done
+                for (( i=1; i<=$ceiling_result; i++ )) 
+                do
+                            testdata=`cat $UPDATE_FILE_PAGINATION-$i.json | jq ".data[$j]"`
+                            echo "$testdata"
+                            : "${put_count:=0}"
+                            : "${post_count:=0}"
+                            while read line
+                            do
+                                result_key=`echo $line | cut -d ':' -f 1`
+                                result_value=`echo $line | cut -d ':' -f 2`
+                                h2=`cat oldpolicyconditionlist | grep -w $result_key`
+                                if [ "$?" == "0" ]
+                                then
+                                    #TODO: Later we can simply skip the PUT if name is common in from and to POLICies.. 
+                                    if [ "$result_value" != "null" ]
+                                    then
+                                        echo "Ietration for page "[ $i ]" for PUT..."
+                                        echo "+++++++++++++++++=PUT++++++++++++++++++"
+                                        local result=`curl -X PUT "https://infra-api.newrelic.com/v2/alerts/conditions/$result_value" -H "X-Api-Key:$NEW_RELIC_ANOTHER_AC" -i -H 'Content-Type: application/json' -d "{\"data\": $testdata}"`
+                                        #`expr "$put_count + 1"`
+                                        put_count=`echo "$put_count + 1" | bc`
+                                    fi
+                                else
+                                    echo "Ietration for page "[ $i ]" for POST..."
+                                    echo $line >> post-should-not-happen.log 
+                                    echo "======================POST==================="
+                                    local result=$(curl -X POST 'https://infra-api.newrelic.com/v2/alerts/conditions' -H "X-Api-Key:$NEW_RELIC_ANOTHER_AC" -i -H 'Content-Type: application/json' -d "{\"data\": $testdata}")
+                                    #`expr "$post_count + 1"`
+                                    post_count=`echo "$post_count + 1" | bc`
+                                fi
+                            done < newpolicyconditionlist-$i
+                        echo "Total PUT call for Page "[ $i ]": $put_count"
+                        echo "Total POST call for Page "[ $i ]": $post_count"
+                done
+        else
+                $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ONE_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$fromoneacpolicyid" > $FILE)
+                #loop_number=`cat $FILE | python -m json.tool | grep -w type | wc -l`
+                loop_number=`cat $FILE | python -m json.tool | jq '.meta.total'`
+                echo "loop_number: $loop_number"
+                ceiling_result=`echo "($loop_number + $no_of_record_per_page - 1) / $no_of_record_per_page" | bc`
+                echo "ceiling_result: $ceiling_result"
+                for (( i=1; i<=$ceiling_result; i++ ))
+                do
+                        $(curl -v -X GET --header "X-Api-Key: $NEW_RELIC_ONE_AC" "https://infra-api.newrelic.com/v2/alerts/conditions?policy_id=$fromoneacpolicyid&offset=$offsetfrom&list=50" > $FILE_PAGINATION-$i.json)
+                        offsetfrom=`echo "$offset + 50" | bc`
+                        echo "offsetfrom: $offsetfrom"
+                        h3=`cat $FILE_PAGINATION-$i.json | sed "s/$fromoneacpolicyid/$toanotheracpolicyid/g " > $UPDATE_FILE_PAGINATION-$i.json`
+                        loop=`cat $UPDATE_FILE_PAGINATION-$i.json | python -m json.tool | grep -w type | wc -l`
+                        for (( j=0; j<$loop; j++ ))
+                        do
+                                testdata=`cat $UPDATE_FILE_PAGINATION-$i.json | jq ".data[$j]"`
+                                echo "======================POST==================="
+                                result=$(curl -v -X POST 'https://infra-api.newrelic.com/v2/alerts/conditions' -H "X-Api-Key:$NEW_RELIC_ANOTHER_AC" -i -H 'Content-Type: application/json' -d "{\"data\": $testdata}")
+                                echo "No of condiftion created for policy id: $toanotheracpolicyid is" `expr $j + 1` 
+                        done
+                done 
+        fi
 }
 cleanup() {
     echo "cleaning up started.."
-    `rm -f $CONDITION_LIST_FOR_TO_POLICY $FILE $UPDATE_FILE oldpolicyconditionlist newpolicyconditionlist`
+    #`rm -f $CONDITION_LIST_FOR_TO_POLICY $FILE $UPDATE_FILE oldpolicyconditionlist newpolicyconditionlist`
+    `rm -f *.json oldpolicyconditionlist newpolicyconditionlist-*`
     echo "cleanup done"
 }
 check_args() {
